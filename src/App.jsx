177dd39a28e30f80355e31624fb62d298782b46e
Raheem Mubarak <raheemmubarak7@gmail.com>
2025-05-05 00:38:11 +0100

    change

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { LoadingScreen } from "./components/LoadingScreen";
 import { Navbar } from "./components/Navbar";
 import { MobileMenu } from "./components/MobileMenu";
 import { Home } from "./components/sections/Home";
-//import { About } from "./components/sections/About";
+import { About } from "./components/sections/About";
 //import { Projects } from "./components/sections/Projects";
 import "./index.css";
 //import { Contact } from "./components/sections/Contact";
@@ -24,8 +24,8 @@ function App() {
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <Home />
-        {/* <About />
-        <Projects />
+        <About />
+        {/* <Projects />
         <Contact />  */}
       </div>
     </>
---
+++ src/components/sections/About.jsx
@@ -0,0 +1,88 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
+export const About = () => {
+  const frontendSkills = [
+    "React",
+    "HTML",
+    "CSS",
+    "JavaScript"
+  ];
+
+
+  return (
+    <section
+      id="about"
+      className="min-h-screen flex items-center justify-center py-20"
+    >
+      <RevealOnScroll>
+        <div className="max-w-3xl mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-gray-500 to-purple-400 bg-clip-text text-transparent text-center">
+            {" "}
+            About Me
+          </h2>
+
+          <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+            <p className="text-gray-300 mb-6">
+              Passionate developer with expertise in building scalable web
+              applications and creating innovative solutions.
+            </p>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4"> Frontend</h3>
+                <div className="flex flex-wrap gap-2">
+                  {frontendSkills.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-gray-500/10 text-purple-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition
+                    "
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
+            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4"> 🏫 Education </h3>
+              <ul className="list-disc list-inside text-purple-300 space-y-2">
+                <li>
+                  <strong> B.Sc in Computer Science </strong> - Lagos State University
+                  (2024-present)
+                </li>
+              </ul>
+            </div>
+            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4"> 💼 Work Experience </h3>
+              <div className="space-y-4 text-purple-300">
+                <div>
+                  <h4 className="font-semibold">
+                    {" "}
+                    Student at Freecodecamp (2021 - 2024){" "}
+                  </h4>
+                  <p>
+                    Learnt frontend development and created some projects along the way.
+                  </p>
+                </div>
+
+                <div>
+                  <h4 className="font-semibold">
+                    {" "}
+                    Intern at Bookers International School (2023-2024){" "}
+                  </h4>
+                  <p>
+                    Assisted in building the school's website and helped manage it.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </RevealOnScroll>
+    </section>
+  );
+};
